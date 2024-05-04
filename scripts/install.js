76b306707e803cae15fe743057453d1d0228697b
mizdra <pp.mizdra@gmail.com>
2024-05-04 17:33:41 +0900

    fix for windows

--- scripts/install.js
+++ scripts/install.js
@@ -3,13 +3,14 @@ import { join } from "path";
 
 const rootDirPath = join(import.meta.dirname, "..");
 
-// NOTE: Permissions determine whether a file is executable or not on Linux.
-// However, the extension determines whether a file is executable or not on Windows.
-// Therefore, for windows, the extension is set to `.exe`.
-const binaryDistPath = join(rootDirPath, "bin/node-run-all.exe");
-
 if (process.platform === "win32") {
-  await cp(join(rootDirPath, 'target/x86_64-pc-windows-gnu/release/node-run-all.exe'), binaryDistPath);
+  // NOTE: Permissions determine whether a file is executable or not on Linux.
+  // However, the extension determines whether a file is executable or not on Windows.
+  // Therefore, for windows, the extension is set to `.exe`.
+  //
+  // Executing `npx node-run-all` on Windows, `exec "node_modules\\.bin\\@mizdra\\node-run-all\\bin\\node-run-all"` be called.
+  // It is then fallback by `PATHEXT` to the "node_modules\\.bin\\@mizdra\\node-run-all\\bin\\node-run-all.exe".
+  await cp(join(rootDirPath, 'target/x86_64-pc-windows-gnu/release/node-run-all.exe'), join(rootDirPath, "bin/node-run-all.exe"));
 } else if (process.platform === "darwin") {
-  await cp(join(rootDirPath, 'target/aarch64-apple-darwin/release/node-run-all'), binaryDistPath);
+  await cp(join(rootDirPath, 'target/aarch64-apple-darwin/release/node-run-all'), join(rootDirPath, "bin/node-run-all"));
 }
