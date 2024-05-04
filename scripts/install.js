1bf0b8cae387bb2bec997e61f00ec78fad96a144
mizdra <pp.mizdra@gmail.com>
2024-05-04 17:12:24 +0900

    fix for windows

--- scripts/install.js
+++ scripts/install.js
@@ -2,7 +2,11 @@ import { cp } from "fs/promises";
 import { join } from "path";
 
 const rootDirPath = join(import.meta.dirname, "..");
-const binaryDistPath = join(rootDirPath, "bin/node-run-all");
+
+// NOTE: Permissions determine whether a file is executable or not on Linux.
+// However, the extension determines whether a file is executable or not on Windows.
+// Therefore, for windows, the extension is set to `.exe`.
+const binaryDistPath = join(rootDirPath, "bin/node-run-all.exe");
 
 if (process.platform === "win32") {
   await cp(join(rootDirPath, 'target/x86_64-pc-windows-gnu/release/node-run-all.exe'), binaryDistPath);
